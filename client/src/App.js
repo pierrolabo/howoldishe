52760a8b6af8f73a59e18db6c0397ed38b6689a3
pierrolabo <pierrolabo@gmail.com>
2021-03-05 23:23:35 +0100

    Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,31 @@
+import { Switch, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
-import {useEffect, useState} from 'react';
 import Home from './pages/Home';
+import Movie from './pages/Movie';
+import NoMatch from './pages/NoMatch';
 
 const axios = require('axios');
+
 const App = () => {
-  const [info, setInfo] = useState("nothing");
+  const [info, setInfo] = useState('nothing');
   const URL = 'http://localhost:2000/api/movies';
-  
+
   return (
     <div className="App">
-      <Home/>
+      <Switch>
+        <Route strict path="/movie/:id">
+          <Movie />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="*">
+          <NoMatch />
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
