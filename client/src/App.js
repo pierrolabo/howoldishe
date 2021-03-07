9a21e3e7ecd8a34e18fbaa75efe0b056d778faaa
pierrolabo <pierrolabo@gmail.com>
2021-03-07 16:04:27 +0100

    refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,10 @@
 import { Switch, Route } from 'react-router-dom';
-import { useEffect, useState } from 'react';
 
 import Home from './pages/Home';
 import Movie from './pages/Movie';
 import NoMatch from './pages/NoMatch';
 
-const axios = require('axios');
-
 const App = () => {
-  const [info, setInfo] = useState('nothing');
-  const URL = 'http://localhost:2000/api/movies';
-
   return (
     <div className="App">
       <Switch>
