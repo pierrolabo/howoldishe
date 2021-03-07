74d615fdfb895a6ac796efe6593d11873a21a806
pierrolabo <pierrolabo@gmail.com>
2021-03-07 16:09:40 +0100

    search useContext

--- client/src/component/Search/Search.js
+++ client/src/component/Search/Search.js
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import axios from 'axios';
+
+import { useState, useContext } from 'react';
+import { MovieListContext } from '../../hooks/MovieList';
+import { API_URL } from '../../constants/Constants';
 
 //  BUG FIX "M is not defined"
-import M from 'materialize-css';
 import 'materialize-css/dist/css/materialize.min.css';
 import { TextInput, Col } from 'react-materialize';
 
@@ -9,6 +12,39 @@ import './Search.scss';
 
 const Search = () => {
   const [search, setSearch] = useState('');
+  const [
+    movies,
+    setMovies,
+    isLoading,
+    setLoading,
+    testVal,
+    setTestVal,
+  ] = useContext(MovieListContext);
+
+  const doSearch = (input) => {
+    //  Prevent sending request when user is erasing input and there's no letters left.
+    if (input === '') return null;
+    setLoading(true);
+    try {
+      axios
+        .get(`${API_URL}/${input}`)
+        .then((res) => {
+          setMovies(res.data.results);
+          setLoading(false);
+        })
+        .catch(function (error) {
+          setLoading(false);
+          console.log(error);
+        });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const handleSearch = (search) => {
+    setSearch(search);
+    const input = search;
+    doSearch(input);
+  };
   return (
     <Col xs={8} s={12} m={12} l={9} className="search valign-wrapper">
       <TextInput
@@ -20,7 +56,7 @@ const Search = () => {
         l={8}
         xl={8}
         value={search}
-        onChange={(e) => setSearch(e.target.value)}
+        onChange={(e) => handleSearch(e.target.value)}
       />
     </Col>
   );
