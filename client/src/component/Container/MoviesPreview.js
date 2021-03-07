b4710424d162fd4ee8d38f28b749e174ce1063e0
pierrolabo <pierrolabo@gmail.com>
2021-03-07 16:05:54 +0100

    refactor

---
+++ client/src/component/Container/MoviesPreview.js
@@ -0,0 +1,34 @@
+import { useContext } from 'react';
+import { MovieListContext } from '../../hooks/MovieList';
+import 'materialize-css/dist/css/materialize.min.css';
+import MovieCardPreview from '../MovieCards/MovieCardPreview';
+import { Row, Preloader } from 'react-materialize';
+
+import './MoviesPreview.scss';
+const MoviesPreview = () => {
+  const [movies, setMovies, isLoading, setLoading] = useContext(
+    MovieListContext
+  );
+  return (
+    <Row className="moviespreview">
+      {isLoading ? (
+        <Preloader
+          active
+          color="blue"
+          flashing={false}
+          size="big"
+          className="moviespreview--loader"
+        />
+      ) : null}
+      {movies?.length === 0 ? (
+        <h1 className="moviespreview--noresults">No results found</h1>
+      ) : null}
+      {!isLoading &&
+        movies &&
+        movies.map((item) => {
+          return <MovieCardPreview {...item} />;
+        })}
+    </Row>
+  );
+};
+export default MoviesPreview;
