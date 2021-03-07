1858eabf5dbae9315ab7f1d1051cbc267bf1940a
pierrolabo <pierrolabo@gmail.com>
2021-03-07 16:07:18 +0100

    refactor

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,13 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-
 import 'materialize-css/dist/css/materialize.min.css';
 import { Row } from 'react-materialize';
+import { MovieListProvider } from '../hooks/MovieList';
+
 import Search from '../component/Search/Search';
-import MovieCardPreview from '../component/MovieCards/MovieCardPreview';
+import MoviesPreview from '../component/Container/MoviesPreview';
 
 import './Home.scss';
-const URL = 'http://localhost:2000/api/movies';
 
 const Home = () => {
-  const [topMovies, setTopMovies] = useState(['a', 'b']);
-  useEffect(() => {
-    try {
-      axios.get(`${URL}/top`).then((res) => setTopMovies(res.data.results));
-    } catch (err) {
-      console.log(err);
-    }
-  }, []);
   return (
     <section className="home">
       <Row>
@@ -30,18 +20,13 @@ const Home = () => {
           Search how old he was in the movie !!
         </h4>
       </Row>
-      <Row>
-        <Search />
-      </Row>
-      <Row style={{ padding: '0rem 2rem' }}>
-        {topMovies &&
-          topMovies.map((item) => {
-            return <MovieCardPreview {...item} />;
-          })}
-        <MovieCardPreview />
-      </Row>
+      <MovieListProvider>
+        <Row>
+          <Search />
+        </Row>
+        <MoviesPreview />
+      </MovieListProvider>
     </section>
   );
 };
-
 export default Home;
