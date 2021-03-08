8d8e81ae9a311a0143494761d5d0de054b39e416
pierrolabo <pierrolabo@gmail.com>
2021-03-08 09:29:04 +0100

    updt url

--- client/src/constants/Constants.js
+++ client/src/constants/Constants.js
@@ -1,5 +1,4 @@
 export const IMG_URL = 'https://image.tmdb.org/t/p';
 export const IMG_SIZE = 'w200';
 export const IMG_SIZE_POSTER = 'w300_and_h450_bestv2';
-export const API_URL = 'http://localhost:2000/api/movies';
-export const API_ROOT = 'http://localhost:2000/api';
+export const API_URL = 'https://localhost:2000/api/movies';
