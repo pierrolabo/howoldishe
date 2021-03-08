e9384f7de41981849517821a96899e22db87fae6
pierrolabo <pierrolabo@gmail.com>
2021-03-08 10:18:29 +0100

    fix ?

--- client/setupProxy.js
+++ client/setupProxy.js
@@ -1,5 +1,11 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
-  app.use(proxy(['/'], { target: 'http://localhost:2000' }));
+  app.use(
+    '/',
+    createProxyMiddleware({
+      target: 'http://localhost:2000',
+      changeOrigin: true,
+    })
+  );
 };
